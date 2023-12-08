c7690ed8fd73ee49c358ddec716b7903cb1d4955
Олег Викторович Гулюкин <67539583+ogulyukin@users.noreply.github.com>
2023-12-08 19:52:17 +0300

    Update LevelInstaller.cs

--- Assets/Scripts/LevelInstaller.cs
+++ Assets/Scripts/LevelInstaller.cs
@@ -2,5 +2,10 @@ using Zenject;
 
 public class LevelInstaller : MonoInstaller
 {
+  [SerializeField] private CharacterView _characterView;
 
+    public override void InstallBindings()
+    {
+        Container.Bind<CharacterView>().FromInstance(_characterView).AsCached();
+    }  
 }
